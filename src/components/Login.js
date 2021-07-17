3f5eabab588f399c2c499315cb0bfc76efef135f
Durrell Jules <durrelljules@Durrells-Mini.attlocal.net>
2021-07-16 20:24:39 -0400

    created login form in Login.js, setup up state and change handler, parts working as intended

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  // Component level state
+  const [formValues, setFormValues] = useState({
+    username:"",
+    password: ""
+  })
+
+  // Change handler
+  const handleChange = e => {
+    setFormValues({
+      ...formValues,
+      [e.target.name]: e.target.value
+    })
+  }
+
+
+  // Handle submit function
+
+
+
 
   const error = "";
   //replace with error state
@@ -11,7 +31,25 @@ const Login = () => {
     <div>
       <h1>Welcome to the Bubble App!</h1>
       <div data-testid="loginForm" className="login-form">
-        <h2>Build login form here</h2>
+        <form>
+          <input 
+          type="text"
+          id="username"
+          name="username"
+          placeholder="username"
+          value={formValues.username}
+          onChange={handleChange}
+          />
+          <input 
+          type="password"
+          id="password"
+          name="password"
+          placeholder="password"
+          value={formValues.password}
+          onChange={handleChange}
+          />
+          <button>Login</button>
+        </form>
       </div>
 
       <p id="error" className="error">{error}</p>
